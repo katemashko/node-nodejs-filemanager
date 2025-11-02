049d4a237bed1126c1da1cdb32c4d610ddf089c9
Katsiaryna Mashko <katemashko@gmail.com>
2025-11-03 00:07:53 +0300

    feat: add decompress command

--- commands/archive/index.js
+++ commands/archive/index.js
@@ -46,6 +46,31 @@ async function decompress(sourceFilePath, newDestinationPath) {
   if (!sourceFilePath || !newDestinationPath) {
     throw new Error("Invalid input");
   }
+
+  const sourceFullFilePath = path.resolve(
+    navigation.getCurrentWorkingDirectory(),
+    sourceFilePath
+  );
+
+  const newFullDestinationPath = path.resolve(
+    navigation.getCurrentWorkingDirectory(),
+    newDestinationPath
+  );
+
+  if (!(await navigation.fileExists(sourceFullFilePath))) {
+    throw new Error("Source file does not exist");
+  }
+
+  if (await navigation.fileExists(newFullDestinationPath)) {
+    throw new Error("File already exists");
+  }
+
+  const brotli = zlib.createBrotliDecompress();
+  const sourceFile = fs.createReadStream(sourceFullFilePath);
+  const destinationFile = fs.createWriteStream(newFullDestinationPath);
+
+  await asyncPipeline(sourceFile, brotli, destinationFile);
+  console.log(`File was decompressed to ${newFullDestinationPath}`);
 }
 
 export { compress, decompress };
